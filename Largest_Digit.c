4ac24de4c8155d93aa46f792bea373c0e7b927d2
Parvathi2005 <119875867+Parvathi2005@users.noreply.github.com>
2023-05-24 22:24:19 +0530

    #include<stdio.h> int main () {     int n,rem,big= 0;     scanf ("%d",&n);     while (n>0)     {         rem=n%10;         if (big<rem)         {             big=rem;         }         n=n/10;     }     printf ("%d", big); }

---
+++ Largest_Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int n,rem,big= 0;
+    scanf ("%d",&n);
+    while (n>0) 
+    {
+        rem=n%10;
+        if (big<rem)
+        {
+            big=rem;
+        }
+        n=n/10;
+    }
+    printf ("%d", big);
+}
